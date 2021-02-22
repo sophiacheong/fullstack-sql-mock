263e4d7b277845b5780c7400fc7521f27bd59c84
sophiacheong <sophiaacheong@gmail.com>
2021-02-21 16:39:40 -0800

    created db connection

--- db/index.js
+++ db/index.js
@@ -1,1 +1,15 @@
-// build your database
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'eBay'
+});
+
+db.connect((err) => {
+  if (err) console.log(err)
+  else console.log('success')
+});
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -2,24 +2,30 @@
 // FIX ME :(
   const express = require('express');
   const path = require('path');
+  const morgan = require('morgan');
+  const bodyparser = require('body-parser');
+  const cors = require('cors');
   const router = require('./router');
 
   const server = express();
-  
+
+  server.use(cors());
+  server.use(morgan('dev'));
+  server.use(bodyparser.json());
   server.use('/', express.static(path.join(__dirname + '/../client/dist')));
-  
+
   server.get('/name', (req, res) => {
     res.status(200).send('This is your get request, modify this file to use your router!')
   })
-  
+
   server.post('/name', (req, res) => {
     res.status(200).send('This is your post request, modify this file to use your router!')
   })
-  
+
   server.put('/name', (req, res) => {
     res.status(200).send('This is your put request, modify this file to use your router!')
   })
-  
+
   server.delete('/name', (req, res) => {
     res.status(200).send('This is your delete request, modify this file to use your router!')
   })
